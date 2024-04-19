11a1ea0bbaaa61dda784cef5f4bcbdebbc5009db
GabrielNunees063 <gabriel.nsilva@sptech.school>
2024-04-18 21:17:58 -0300

    Criação dinamica dos cards do jogos

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,4 @@
-async function buscar(){
-    console.log("Passei por aqui")
+async function buscar() {
     const resposta = await fetch("https://660f44bc356b87a55c510f18.mockapi.io/agendas")
     const respostaDadosAgenda = await resposta.json()
     console.log("Resposta: ", respostaDadosAgenda)
@@ -7,10 +6,30 @@ async function buscar(){
     const cards = document.getElementById("cards_games")
 
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-       return "teste"
-    });
+        return `
+        <div class="cardItem">
+            <div class="dataGame"> 
+                <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 21:30 
+            </div>
+            <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+            <div class="descricao">
+                <p> <strong>${itemAgenda.nome}</strong> </p>
+                <p> ${itemAgenda.descricao} </p>
+                <p><strong>Gamers: </strong></p>
+
+                <div class="gamers">
+                ${itemAgenda.gamers.map((gamerItem) => {
+                    return `<div class="gamerItem">@${gamerItem}</div>`
+                }).join('')
+            }
+                </div>
+            </div>  
+            <div class="assistir">
+               <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+            </div>
+        </div>
+       `
+    }).join('');
 }
 
-console.log("Antes de buscar")
 buscar()
-console.log("Depois de buscar")
